307d7d6aba6bb3d52a69c7bec2827571e5ec2600
Antonio Maria <45206509+Silcodon@users.noreply.github.com>
2020-12-05 21:39:36 +0000

    update

--- Projeto3Sender/src/common/ClientResearcher.java
+++ Projeto3Sender/src/common/ClientResearcher.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class ClientResearcher {
 	public static void main(String[] args) {
+		String username,password;
+	    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 		boolean done  = false;
 		while(!done) {
 			System.out.println("****************JMS****************");
@@ -15,6 +17,30 @@ public class ClientResearcher {
 
 			int option = lerInt(0, 3);
 			
+			//REGISTER
+			if(option==0) {
+				System.out.println("****************Register JMS****************");
+				System.out.print("Escolha um username: ");
+			    username = scanner.nextLine();  // Read user input
+				System.out.print("Escolha uma password: ");
+				password = scanner.nextLine();  // Read user input
+				System.out.println("\n");
+				//Enviar msg ao admin para confirmar registo
+				
+			}
+			
+			//LOGIN
+			if(option==1) {
+				System.out.println("****************Login JMS****************");
+				System.out.print("Username: ");
+			    username = scanner.nextLine();  // Read user input
+				System.out.print("Password: ");
+				password = scanner.nextLine();  // Read user input
+				System.out.println("\n");
+				//Enviar msg ao admin para login
+				
+				//Após confirmar aceder à app
+			}
 			
 			//EXIT
 			if(option==2) {
@@ -47,5 +73,80 @@ public class ClientResearcher {
 				}
 			}
 			return num;
+		}
+		
+		public void appoption() {
+			boolean done  = false;
+		    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+			String bookname,type,date;
+			while(!done) {
+				System.out.println("****************JMS App****************");
+				System.out.println("Escolha uma opcao: ");
+				System.out.println("(0) List Publications ");
+				System.out.println("(1) Search Publication ");
+				System.out.println("(2) Add Publication ");
+				System.out.println("(3) Update Publication ");
+				System.out.println("(4) Remove Publication ");
+				System.out.println("(5) Logout ");
+				System.out.println("\n");
+
+				int option = lerInt(0, 6);
+				
+				//LIST ALL PUBLICATIONS
+				if(option==0) {
+					//Enviar msg ao admin
+				}
+				
+				
+				//SEARCH PUBLICATION BY NAME
+				if(option==1) {
+					System.out.println("****************SEARCH JMS****************");
+					System.out.print("Nome da publicacao: ");
+				    bookname = scanner.nextLine();  // Read user input
+					System.out.println("\n");
+					//Enviar mensagem a pedir info ao admin
+				}
+				
+				//ADD PUBLICATION
+				if(option==2) {
+					System.out.println("****************ADD JMS****************");
+					System.out.print("Nome da publicacao: ");
+				    bookname = scanner.nextLine();  // Read user input
+				    System.out.print("Tipo da publicacao: ");
+				    type = scanner.nextLine();  // Read user input
+				    System.out.print("Data da publicacao (Month Year): ");
+				    date = scanner.nextLine();  // Read user input
+					System.out.println("\n");
+					//Enviar mensagem a pedir info ao admin
+				}
+				
+				//UPDATE PUBLICATION
+				if(option==3) {
+					System.out.println("****************UPDATE JMS****************");
+					System.out.print("Nome da publicacao: ");
+				    bookname = scanner.nextLine();  // Read user input
+				    System.out.print("Tipo da publicacao: ");
+				    type = scanner.nextLine();  // Read user input
+				    System.out.print("Data da publicacao (Month Year): ");
+				    date = scanner.nextLine();  // Read user input
+					System.out.println("\n");
+					//Enviar mensagem a pedir info ao admin
+				}
+				
+				//REMOVE PUBLICATION
+				if(option==4) {
+					System.out.println("****************REMOVE JMS****************");
+					System.out.print("Nome da publicacao: ");
+				    bookname = scanner.nextLine();  // Read user input
+					System.out.println("\n");
+					//Enviar mensagem a pedir info ao admin
+				}
+				
+				//EXIT
+				if(option==5) {
+					System.out.println("You have successfully logged out.");
+	                done = true;
+				}
+			}
 		}
 }
