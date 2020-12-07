ae458157e646cc393cd905b24fe344f280efc49f
Antonio Maria <45206509+Silcodon@users.noreply.github.com>
2020-12-07 23:39:51 +0000

    PendingTasks

--- Projeto3Sender/src/common/ClientResearcher.java
+++ Projeto3Sender/src/common/ClientResearcher.java
@@ -1,6 +1,11 @@
 package common;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
@@ -20,12 +25,11 @@ import classes.AsyncReceiver;
 import classes.Publication;
 import classes.Sender;
 import classes.User;
-import classes.Publication;
 
 public class ClientResearcher {
-
-	public static void main(String[] args) throws NamingException {
+	public static void main(String[] args) {
 		String username,password;
+		boolean validusername,validpassword;
 	    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 		boolean done  = false;
 		while(!done) {
@@ -37,32 +41,40 @@ public class ClientResearcher {
 			System.out.println("\n");
 
 			int option = lerInt(0, 3);
-
+			
 			//REGISTER
 			if(option==0) {
-				/*System.out.println("****************Register JMS****************");
+				System.out.println("****************Register JMS****************");
 				System.out.print("Escolha um username: ");
 			    username = scanner.nextLine();  // Read user input
 				System.out.print("Escolha uma password: ");
 				password = scanner.nextLine();  // Read user input
-				User utilizador = new User(username,password);*/
-				//Enviar msg ao admin para confirmar registo
-				sendmessage("SEND");
+				System.out.println("\n");
+				validusername = (username != null) && username.matches("[A-Za-z0-9_]+");
+				validpassword = (password != null) && password.matches("[A-Za-z0-9_]+");
+				if(validusername==false || validpassword==false) {
+					System.out.println("Please enter a valid username and password");
+				}
+				else {
+					User utilizador = new User(username,password);
+					//Enviar msg ao admin para confirmar registo	
+				}
+				
 			}
-
+			
 			//LOGIN
 			if(option==1) {
-				/*System.out.println("****************Login JMS****************");
+				System.out.println("****************Login JMS****************");
 				System.out.print("Username: ");
 			    username = scanner.nextLine();  // Read user input
 				System.out.print("Password: ");
 				password = scanner.nextLine();  // Read user input
-				System.out.println("\n");*/
+				System.out.println("\n");
 				//Enviar msg ao admin para login
-				printresponse();
+				
 				//Ap�s confirmar aceder � app
 			}
-
+			
 			//EXIT
 			if(option==2) {
 				System.out.println("You have successfully exited the Application.");
@@ -70,8 +82,8 @@ public class ClientResearcher {
 			}
 		}
 	}
-
-
+	
+	
 	//LER INT DE STDIN
 		public static int lerInt(int init, int fin ) {
 			Scanner scan = new Scanner(System.in);
@@ -95,16 +107,13 @@ public class ClientResearcher {
 			}
 			return num;
 		}
-
-
+		
+		
 		//MENU DE USER
-		public void appuser() throws NamingException {
+		public void appuser() {
 			boolean done  = false;
 		    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 			String bookname,type,date;
-
-			Sender playQueue = new Sender();
-
 			while(!done) {
 				System.out.println("****************JMS App****************");
 				System.out.println("Escolha uma opcao: ");
@@ -117,15 +126,13 @@ public class ClientResearcher {
 				System.out.println("\n");
 
 				int option = lerInt(0, 6);
-
+				
 				//LIST ALL PUBLICATIONS
 				if(option==0) {
 					//Enviar msg ao admin
-
-
 				}
-
-
+				
+				
 				//SEARCH PUBLICATION BY NAME
 				if(option==1) {
 					System.out.println("****************SEARCH JMS****************");
@@ -134,7 +141,7 @@ public class ClientResearcher {
 					System.out.println("\n");
 					//Enviar mensagem a pedir info ao admin
 				}
-
+				
 				//ADD PUBLICATION
 				if(option==2) {
 					System.out.println("****************ADD JMS****************");
@@ -147,7 +154,7 @@ public class ClientResearcher {
 					System.out.println("\n");
 					//Enviar mensagem a pedir info ao admin
 				}
-
+				
 				//UPDATE PUBLICATION
 				if(option==3) {
 					System.out.println("****************UPDATE JMS****************");
@@ -162,7 +169,7 @@ public class ClientResearcher {
 					System.out.println("\n");
 					//Enviar mensagem a pedir info ao admin
 				}
-
+				
 				//REMOVE PUBLICATION
 				if(option==4) {
 					System.out.println("****************REMOVE JMS****************");
@@ -171,7 +178,7 @@ public class ClientResearcher {
 					System.out.println("\n");
 					//Enviar mensagem a pedir info ao admin
 				}
-
+				
 				//EXIT
 				if(option==5) {
 					System.out.println("You have successfully logged out.");
@@ -179,17 +186,16 @@ public class ClientResearcher {
 				}
 			}
 		}
-
-
-
+		
+		
+		
 		//MENU DE ADMIN
-		public void appadmin() throws NamingException {
+		public void appadmin() {
 			boolean done  = false;
 		    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+		    ArrayList<String> tasks = new ArrayList<String>();
+		    tasks=ReadFile();
 			String user,bookname;
-			//criar queues
-
-
 			while(!done) {
 				System.out.println("****************JMS App****************");
 				System.out.println("Escolha uma opcao: ");
@@ -202,18 +208,24 @@ public class ClientResearcher {
 				System.out.println("\n");
 
 				int option = lerInt(0, 6);
-
+				
 				//LIST ALL USERS
 				if(option==0) {
-
+					
 				}
-
-
+						
+						
 				//LIST PENDING TASKS
 				if(option==1) {
-
+					System.out.println("****************TASKS JMS****************");
+					PrintTasks(tasks);
+					System.out.print("Choose a pending task (Type 0 to exit): ");
+					int option2=lerInt(0,tasks.size()+1);
+					if(option2!=0) {
+						SelectTask(tasks,option2-1);
+					}
 				}
-
+						
 				//DEACTIVATE USER
 				if(option==2) {
 					System.out.println("****************DEACTIVATE JMS****************");
@@ -221,12 +233,12 @@ public class ClientResearcher {
 				    user = scanner.nextLine();  // Read user input
 					System.out.println("\n");
 				}
-
+						
 				//LIST PUBLICATIONS
 				if(option==3) {
-
+							
 				}
-
+						
 				//SEARCH PUBLICATION
 				if(option==4) {
 					System.out.println("****************SEARCH JMS****************");
@@ -234,7 +246,7 @@ public class ClientResearcher {
 				    bookname = scanner.nextLine();  // Read user input
 					System.out.println("\n");
 				}
-
+						
 				//EXIT
 				if(option==5) {
 					System.out.println("You have successfully logged out.");
@@ -242,44 +254,42 @@ public class ClientResearcher {
 				}
 			}
 		}
+		
+		
+		
+//=========================JMS AUX=============================================================		
 
-
-
-//=========================JMS AUX=============================================================
-
-
+		
 		//RECEIVE A MESSAGE
-		public static void printresponse() throws NamingException {
 			AsyncReceiver asyncReceiver = new AsyncReceiver();
 			asyncReceiver.launch_and_wait();
 		}
-
+		
 		//RECEIVE AND REPLY
 		public void printandrespond() throws NamingException, JMSException {
 			AsyncReceiver asyncReceiver = new AsyncReceiver();
 			asyncReceiver.receive_and_reply();
 		}
-
-
+		
+		
 		//SEND A MESSAGE
-		public static void sendmessage(String message) throws NamingException {
 			Sender sender = new Sender();
 			sender.send(message);
 		}
-
+		
 		//SEND MESSAGE AND PRINT RESPONSE
 		public void sendandreceive(String message) throws NamingException {
 			Sender sender = new Sender();
 			sender.send_and_reply(message);
 		}
-
-
-
-
-//=========================GET DATABASE INFO=============================================================
-
-
-
+		
+		
+		
+		
+//=========================GET DATABASE INFO=============================================================		
+		
+		
+		
 		//GET ALL USERS
 		public static List<User> GetallUsers(){
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
@@ -292,7 +302,7 @@ public class ClientResearcher {
 		    List<User> mylist = typedQuery.getResultList();
 		    return mylist;
 		 }
-
+		
 		//GET ALL PUBS
 		public static List<Publication> GetallPubs(){
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
@@ -305,11 +315,8 @@ public class ClientResearcher {
 		    List<Publication> mylist = typedQuery.getResultList();
 		    return mylist;
 		 }
-
-
-
-
-		//GET PUBS BY NOME
+		    
+		//GET PUBS BY NOME	
 		public List<Publication> GetPublicationByNome(String nome){
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
 			EntityManager em = emfactory.createEntityManager( );
@@ -323,7 +330,7 @@ public class ClientResearcher {
 			List<Publication> mylist = typedQuery.getResultList();
 			return mylist;
 		}
-
+		
 		//ADD USER
 		public void AddUser(User st) {
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
@@ -338,7 +345,7 @@ public class ClientResearcher {
 			entitymanager.close( );
 			emfactory.close( );
 		}
-
+		
 		//ADD PUBLICATION
 		public void AddPublication(Publication st) {
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
@@ -349,9 +356,9 @@ public class ClientResearcher {
 			entitymanager.close( );
 			emfactory.close( );
 		}
-
+		
 		//UPDATE PUBLICATION
-		public void UpdatePublication(Publication st, Publication stold) {
+		public void UpdatePublication(Publication st, String old) {
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
 			EntityManager em = emfactory.createEntityManager( );
 		    em.getTransaction().begin();
@@ -359,39 +366,39 @@ public class ClientResearcher {
 		    	Query query = em.createQuery("UPDATE Publication p SET p.name =: nome"
 			            + "WHERE p.name = :oldname");
 		    	query.setParameter("nome", st.getName());
-			    query.setParameter("oldname", stold.getName());
+			    query.setParameter("oldname", old);
 			    query.executeUpdate();
 		    }
 		    if(st.getType().compareTo("")!=0) {
 		    	Query query = em.createQuery("UPDATE Publication p SET p.type =: tipo "
 			            + "WHERE p.name = :oldname");
 		    	query.setParameter("tipo", st.getType());
-			    query.setParameter("oldname", stold.getName());
+			    query.setParameter("oldname", old);
 			    query.executeUpdate();
 		    }
 		    if(st.getDate().compareTo("")!=0) {
 		    	Query query = em.createQuery("UPDATE Publication p SET p.date =: data "
 			            + "WHERE p.name = :oldname");
 		    	query.setParameter("data", st.getDate());
-			    query.setParameter("oldname", stold.getName());
+			    query.setParameter("oldname", old);
 			    query.executeUpdate();
-		    }
+		    }	    
 		    em.getTransaction().commit();
 		    em.close();
 		}
-
+		
 		//REMOVE A PUBLICATION
-		public void RemovePublication(Publication st) {
+		public void RemovePublication(String pubname) {
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
 			EntityManager em = emfactory.createEntityManager( );
 			em.getTransaction().begin();
 		    Query query = em.createQuery("DELETE FROM Publication p WHERE p.name = :Name ");
-		    query.setParameter("Name", st.getName());
+		    query.setParameter("Name", pubname);
 		    query.executeUpdate();
 		    em.getTransaction().commit();
 		    em.close();
 		}
-
+		
 		//ACTIVATE OR DEACTIVATE A USER
 		public void ActivateUser(User st) {
 			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
@@ -409,13 +416,31 @@ public class ClientResearcher {
 			    query.setParameter("Name", st.getUsername());
 			    query.executeUpdate();
 			}
-
+			
 		    em.getTransaction().commit();
-		    em.close();
+		    em.close();	
 		}
-
-
-//=========================PRINT DATABASE INFO=============================================================
+		
+		//LOGIN
+		public User login(String username, String password) {
+			EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Loader" );
+			EntityManager em = emfactory.createEntityManager( );
+			// Define query String
+			String jpql = "SELECT u FROM User u where u.username=:name AND u.password=:pass";
+			// Create a (typed) query
+			TypedQuery<User> typedQuery = em.createQuery(jpql, User.class);
+			// Set parameter
+			typedQuery.setParameter("name", username);
+			typedQuery.setParameter("pass", password);
+			// Query and get result
+			List<User> mylist = typedQuery.getResultList();
+			User user=mylist.get(0);
+			return user;
+			
+		}
+		
+		
+//=========================PRINT DATABASE INFO=============================================================	
 		//PRINT ALL USERS INFO
 		public static void printallusers() {
 			List<User> mylist = GetallUsers();
@@ -425,11 +450,11 @@ public class ClientResearcher {
 			}
 			System.out.println("\n");
 		}
-
+		
 		//PRINT ALL PUBS INFO
 		public static void printallpubs(List<Publication> mylist) {
 			for(int i=0;i<mylist.size();i++) {
-				System.out.println("Publication " + i + ":");
+				System.out.println("Publication " + (i+1) + ":");
 				System.out.println("Name: " + mylist.get(i).getName());
 				System.out.println("Type: " + mylist.get(i).getType());
 				System.out.println("Date: " + mylist.get(i).getDate());
@@ -437,5 +462,118 @@ public class ClientResearcher {
 			}
 			System.out.println("\n");
 		}
-
+		
+		
+//=========================PENDING TASKS AUX=================================================================
+		//READ PENDING TASKS FROM FILE
+		public static ArrayList<String> ReadFile() {
+			ArrayList<String> tasks = new ArrayList<String>();
+			try {
+				File myObj = new File("Tasks.txt");
+				Scanner myReader = new Scanner(myObj);
+				while (myReader.hasNextLine()) {
+					String data = myReader.nextLine();
+					tasks.add(data);
+				}
+				myReader.close();
+		    	} catch (FileNotFoundException e) {
+		    		return tasks;
+		    	}
+			return tasks;
+		 }
+		
+		
+		//WRITE PENDING TASKS TO FILE
+		public void WriteFile(ArrayList<String> tasks) {
+			//CREATE IF IT DOESNT EXIST
+			try {
+				File myObj = new File("Tasks.txt");
+			    myObj.createNewFile();
+			} catch (IOException e) {
+				 System.out.println("An error occurred.");
+			     e.printStackTrace();
+			}
+			//WRITE TO FILE
+			try {
+				BufferedWriter outputWriter = null;
+				outputWriter = new BufferedWriter(new FileWriter("Tasks.txt"));
+				for(int i=0;i<tasks.size();i++) {
+					outputWriter.write(tasks.get(i));
+					outputWriter.newLine();
+				}
+				outputWriter.flush();  
+				outputWriter.close();
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+		
+		//PRINT ALL TASKS
+		public void PrintTasks(ArrayList<String> tasks) {
+			for(int i=0;i<tasks.size();i++) {
+				System.out.println("("+(i+1)+") " + tasks.get(i));
+			}
+		}
+		
+		//SELECT A TASK
+		//RETURNS ARRAYLIST WITH DECLINED TASK OR ACCEPTED
+		public ArrayList<String> SelectTask(ArrayList<String> tasks, int choice) {
+		    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+		    boolean done=false;
+			System.out.println("Task selected: " + tasks.get(choice));
+			while(!done) {
+				System.out.println("Do you want to accept or decline?");
+				System.out.println("Type 'exit' to cancel");
+			    String answer = scanner.nextLine();  // Read user input
+				if(answer.compareTo("accept")==0) {
+					DoTask(tasks.get(choice));
+					done=true;
+				}
+				else if(answer.compareTo("decline")==0) {
+					tasks.remove(choice);
+					done=true;
+				}
+				else if(answer.compareTo("exit")==0) {
+					done=true;
+				}
+				else {
+					System.out.println("Please type 'accept' or 'decline'");
+				}
+			}
+			return tasks;
+		}
+		
+		
+		//PARSE TASK AND EXECUTE
+		public void DoTask(String task) {
+			String[] tokens = task.split(":");
+			//REGISTAR(Registo:Username:Password)
+			if(tokens[0].compareTo("Registo")==0) {
+				User novo= new User(tokens[1],tokens[2]);
+				AddUser(novo);
+				System.out.println("Utilizador " + tokens[1] + " adicionado com sucesso!");
+			}
+			//ADICIONAR(Adicionar:TestePub:Book:March 2013)
+			else if(tokens[0].compareTo("Adicionar")==0) {
+				Publication novo = new Publication(tokens[1],tokens[2],tokens[3]);
+				AddPublication(novo);
+				System.out.println("Publication " + tokens[1] + " adicionada com sucesso!");
+			}
+			//UPDATE(Update:TestePub:TestePubv2:Book:March 2014)
+			else if(tokens[0].compareTo("Update")==0) {
+				Publication updated=new Publication(tokens[2],tokens[3],tokens[4]);
+				String oldname=tokens[1];
+				UpdatePublication(updated,oldname);
+				System.out.println("Publication atualizada!");
+			}
+			//REMOVE(Remover:TestePubv2)
+			else if(tokens[0].compareTo("Remover")==0) {
+				RemovePublication(tokens[1]);
+				System.out.println("Publica��o removida!");
+			}
+			else {
+				System.out.println("Something went wrong with parsing! :(");
+			}
+		}
 }
